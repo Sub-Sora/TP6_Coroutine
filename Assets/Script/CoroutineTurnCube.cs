9018772b9bde9b27402428cf08d790590a59747f
Max <maxime.harry@e-artsup.net>
2024-02-07 20:48:09 +0100

    Work!

---
+++ Assets/Script/CoroutineTurnCube.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class CoroutineTurnCube : MonoBehaviour
+{
+    [SerializeField] private float _rotationSpd;
+    private Coroutine _turnCubeCoroutine;
+
+    IEnumerator TurnCube()
+    {
+        float totalTime = 0f;
+        while (totalTime < 5f)
+        {
+            transform.Rotate(new Vector3(0f, _rotationSpd * Time.deltaTime, 0f));
+            totalTime += Time.deltaTime;
+            yield return null;
+        }
+        _turnCubeCoroutine = null;
+    }
+
+    public void StartTurnCube()
+    {
+        if (_turnCubeCoroutine == null)
+        {
+            _turnCubeCoroutine = StartCoroutine(TurnCube());
+        }
+    }
+
+    public void PauseTurnCube()
+    {
+        if (_turnCubeCoroutine != null)
+        {
+            StopCoroutine(_turnCubeCoroutine);
+            _turnCubeCoroutine = null;
+        }
+    }
+}
---
+++ Assets/Script/UnitaskTurnCube.cs
@@ -0,0 +1,30 @@
+﻿using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+public class UnitaskTurnCube : MonoBehaviour
+{
+    [SerializeField] private float _rotationSpd;
+    private bool _isPlayed = true;
+
+    async UniTask TaskTurnCube()
+    {
+        float totalTime = 0f;
+        while (_isPlayed && totalTime < 5f)
+        {
+            transform.Rotate(new Vector3(0f, _rotationSpd * Time.deltaTime, 0f));
+            await UniTask.Delay(1);
+            totalTime += Time.deltaTime;
+        }
+    }
+
+    public async void StartTaskTurnCube()
+    {
+        _isPlayed = true;
+        await TaskTurnCube();
+    }
+
+    public void PauseTaskTurnCube()
+    {
+        if(_isPlayed) _isPlayed = false;
+    }
+}
